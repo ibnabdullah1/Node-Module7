3339165fac5daa3fe33785d2eba256bf6bfc49cf
abdullah01 <arfathossen541@gmail.com>
2024-05-15 12:28:35 +0600

    Topic : Node.js builting module

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
 // console.log(a);
 
 // Built-in modules
+
+const path = require("path");
+console.log(
+  path.join(
+    "/D:/Next Level Courses/Next Level Web Development/MISSION 2/Module 7/index.js"
+  )
+);
